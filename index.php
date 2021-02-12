45da4930ee8ce7e37845d7df67e0bb71f20fc43d
Corey <57112820+corey613@users.noreply.github.com>
2021-02-12 19:32:28 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php require_once 'process.php'; ?>
+
+
+    
+    <?php 
+           if (isset($_SESSION['message'])):  ?>
+      
+      
+      <div  class="alert alert-<?=$_SESSION['msg_type']?>" >
+     
+    <?php 
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        ?>
+     </div>
+    
+            <?php endif ?>
+
+
+    <?php
+        function pre_r($array){
+            echo '<pre>';
+            print_r($array);
+            echo '</pre>';
+        }
+    ?>
+
+    <div id="formContainer">
+    <form action="process.php" method="POST">
+        <input type="hidden" name="id" value="<?php echo $id;?>">
+
+    <label class="label">Name:</label>
+    <input type="text" name="name" class="input" placeholder="Enter your name"
+     value="<?php echo $name; ?>">
+
+    <label class="label">Age:</label>
+    <input type="text" name="age" class="input"  placeholder="Enter your age"
+    value="<?php echo $age; ?>">
+
+        <?php 
+        if($update == true):
+        ?>
+        <button type="Sumbit" name="update" id="update">Update</button>
+        <?php else: ?>
+        <button type="Sumbit" name="save" id="save">Save</button>
+        <?php endif; ?>
+    </form>
+</div>
+
+        
+
+    <?php
+        $mysqli = new mysqli("localhost", "root", '',"crud") or die(mysqli_error($mysqli));
+        $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+     
+    	 ?>
+
+        <div id="display">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th colspan="2" class="col2">Action</th>
+                    </tr>
+                </thead>
+
+                <?php
+                    while($row = $result->fetch_assoc()): ?>
+               <tr>
+                   <td><?php echo $row["name"]; ?></td>
+                   <td><?php echo $row["age"]; ?></td>
+                   <td class="col2">
+                       <a href="index.php?edit=<?php echo $row['id']; ?> " class="editBtn" >
+                            Edit
+                    </a>
+                    <a href="process.php?delete=<?php echo $row['id']; ?>" id="delete">
+                        Delete
+                    </a>
+                   </td>
+               </tr>
+
+               <?php endwhile; ?>
+
+            </table>
+
+        </div>
+
+
+ 
+    
+  
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,60 @@
+<?php
+
+session_start();
+
+$name= '';
+$age = '';
+$update = false;
+$id = 0;
+
+
+$mysqli = new mysqli("localhost", "root", '',"crud") or die(mysqli_error($mysqli));
+
+if(isset($_POST["save"])){
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+
+    $mysqli->query("INSERT INTO data (name, age) VALUES('$name', '$age')"); 
+       
+    $_SESSION['message'] = 'This record has been saved!';
+    $_SESSION['msg_type'] = 'success';
+
+    header('location: index.php');
+
+}
+
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $mysqli->query("DELETE FROM data WHERE id=$id") or 
+    die($mysqli->error());
+
+    $_SESSION['message'] = 'This record has been deleted!';
+    $_SESSION['msg_type'] = 'delete';
+
+    header('location: index.php');
+}
+
+if(isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $update = true;
+    $result = $mysqli->query("SELECT * FROM data WHERE id=$id");
+
+    if($result &&  $row = $result->fetch_array()){
+        $name = $row['name'];
+        $age = $row["age"];
+    }
+}
+
+
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $name = $_POST['name']; 
+    $age = $_POST['age'];
+    
+    $mysqli->query("UPDATE data SET name='$name', age='$age' WHERE id=$id ");
+    $_SESSION['message'] = 'Record has been updated';
+    $_SESSION['msg_type'] = 'update';
+
+    header('location: index.php');
+   
+}
